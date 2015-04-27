4a82158375b751158622d6a3e6660efadfca1689
Fulvio Casali <fulviocasali@gmail.com>
2015-04-26 20:20:39 -0700

    Bulk-rearrange file structure.

---
+++ client/helpers/site.js
@@ -0,0 +1,9 @@
+// A global helper that returns the site's root URL
+Template.registerHelper("siteroot", Meteor.absoluteUrl(""));
+
+// When loading the page, fix the <base> href attribute.
+Meteor.startup(function () {
+Meteor.autorun(function () {
+    $('base').attr('href', Meteor.absoluteUrl(""));
+});
+});
