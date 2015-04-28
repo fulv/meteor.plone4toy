f07f4e7c46ba20a4699cbb928d0f1b2cb40f9489
Fulvio Casali <fulviocasali@gmail.com>
2015-04-27 22:26:52 -0700

    Add comment to clarify load order.

--- client/compatibility/portal_url.js
+++ client/compatibility/portal_url.js
@@ -1,2 +1,4 @@
-// This needs to be global
+// This needs to be global and be loaded after plone.js
+// It so happens that the current filename comes after
+// plone.js alphabetically, which is why it works.
 portal_url = Meteor.absoluteUrl("");
