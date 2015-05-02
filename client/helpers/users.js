8bb18cc37d47b400d4f098cccc9d4526830d0e79
Fulvio Casali <fulviocasali@gmail.com>
2015-05-02 11:36:33 -0700

    Remove insecure, and debug line.

--- client/helpers/users.js
+++ client/helpers/users.js
@@ -3,6 +3,5 @@ UsersCount = new Mongo.Collection('users-count');
 Meteor.subscribe("userData");
 
 Template.registerHelper('routeData', function() {
-  console.log(this);
   return {author: this.author};
 });
