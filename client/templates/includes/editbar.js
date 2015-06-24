3602daaa5d04089385b068f32e619d0a478645f0
Fulvio Casali <fulviocasali@gmail.com>
2015-06-24 09:49:08 -0700

    Fix the tabClass errors when switching from @@owncontent route to a route that has no editbar.

--- client/templates/includes/editbar.js
+++ client/templates/includes/editbar.js
@@ -2,5 +2,14 @@ Template.EditBar.helpers({
   showBorder : function() {
     console.log('In Editbar.helpers.showBorder:' + Iron.controller().state.get('showBorder'));
     return Iron.controller().state.get('showBorder');
+  },
+  tabClass : function(tab) {
+    console.log('in Editbar.helpers.tabClass');
+    var target = Iron.controller().data();
+    console.log(target);
+    if (typeof target !== 'undefined' && typeof target.tabClass !== 'undefined') {
+      console.log(target.tabClass);
+      return target.tabClass(tab);
+    }
   }
 });
