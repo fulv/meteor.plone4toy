5f7a0135a8e9ec382b504f0f06bcd8073ba1f5c4
Fulvio Casali <fulviocasali@gmail.com>
2015-08-03 23:56:52 -0400

    Fix indentation.

--- client/templates/includes/workflowmenu.js
+++ client/templates/includes/workflowmenu.js
@@ -1,60 +1,59 @@
 Template.PloneContentmenuWorkflow.helpers({
-    activeState: function() {
-        return Session.get('PloneContentmenuWorkflow.active');
-    },
-    workflowState: function() {
-      return this.item.workflow_state;
-    },
-    workflowStateClass: function() {
-      return this.item.workflow_state.toLowerCase();
-    },
-    public: function() {
-      return (this.item.workflow_state === 'Published');
-    },
-    own: function() {
-      if (this.item) {
-        return (!!Meteor.userId() && this.item.author === Meteor.user().username);
-      } else {
-        return false;
-      }
+  activeState: function() {
+    return Session.get('PloneContentmenuWorkflow.active');
+  },
+  workflowState: function() {
+    return this.item.workflow_state;
+  },
+  workflowStateClass: function() {
+    return this.item.workflow_state.toLowerCase();
+  },
+  public: function() {
+    return (this.item.workflow_state === 'Published');
+  },
+  own: function() {
+    if (this.item) {
+      return (!!Meteor.userId() && this.item.author === Meteor.user().username);
+    } else {
+      return false;
     }
+  }
 });
 
 Template.PloneContentmenuWorkflow.events({
-    'click .actionMenuHeader': function(e) {
-        e.preventDefault();
+  'click .actionMenuHeader': function(e) {
+    e.preventDefault();
 
-        Session.set('PloneContentmenuWorkflow.active', 'activated');
-    },
-    'click dl.activated .actionMenuHeader': function(e) {
-        e.preventDefault();
+    Session.set('PloneContentmenuWorkflow.active', 'activated');
+  },
+  'click dl.activated .actionMenuHeader': function(e) {
+    e.preventDefault();
 
-        Session.set('PloneContentmenuWorkflow.active', 'deactivated');
-    },
-    'click dd': function(e) {
-        e.preventDefault();
+    Session.set('PloneContentmenuWorkflow.active', 'deactivated');
+  },
+  'click dd': function(e) {
+    e.preventDefault();
 
-        Session.set('PloneContentmenuWorkflow.active', 'deactivated');
-    },
-    'mouseleave dl': function(e) {
-        Session.set('PloneContentmenuWorkflow.active', 'deactivated');
-    },
-    'click a.kssIgnore': function(e) {
-      e.preventDefault();
+    Session.set('PloneContentmenuWorkflow.active', 'deactivated');
+  },
+  'mouseleave dl': function(e) {
+    Session.set('PloneContentmenuWorkflow.active', 'deactivated');
+  },
+  'click a.kssIgnore': function(e) {
+    e.preventDefault();
 
-      var element = e.target;
-      if (element.nodeName != 'A') {
-          element = e.target.parentNode;
-      }
-      var transition = element.id;
-      var id = this.item._id;
-
-      Meteor.call('itemTransition', id, transition, function(error, result) {
-          if (error) {
-              throwError(error.reason);
-          }
-      });
-
+    var element = e.target;
+    if (element.nodeName != 'A') {
+      element = e.target.parentNode;
     }
+    var transition = element.id;
+    var id = this.item._id;
+
+    Meteor.call('itemTransition', id, transition, function(error, result) {
+      if (error) {
+        throwError(error.reason);
+      }
+    });
+  }
 });
 
